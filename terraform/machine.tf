3d8196135ba313d4c3e725ab88d098b570f04644
Your NameOlaniyi <ayodelebwj@gmail.com>
2026-02-17 12:47:49 -0500

    ps60

--- terraform/machine.tf
+++ terraform/machine.tf
@@ -1,5 +1,5 @@
 //Step 4 — Attach to EC2
-resource "aws_instance" "private_server" {
+resource "aws_instance" "backend_server" {
   ami                    = "ami-0b6c6ebed2801a5cb"
   instance_type          = "t2.micro"
   iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
@@ -21,5 +21,30 @@ resource "aws_instance" "private_server" {
   tags = {
     Name = "backend-instance"
     Role = "backend"
+  }
+}
+
+resource "aws_instance" "frontend_server" {
+  ami                    = "subnet-0e7c30f99b3fd1f62"
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  subnet_id              = data.aws_subnet.public_subnet.id
+
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update system packages
+              sudo apt update -y
+              sudo apt install -y snapd
+              sudo snap install core
+              sudo snap refresh core
+              sudo snap install amazon-ssm-agent --classic
+              sudo snap start amazon-ssm-agent
+              sudo snap enable amazon-ssm-agent
+              EOF
+
+
+  tags = {
+    Name = "frontend-instance"
+    Role = "frontend"
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,21 @@
 output "github_actions_role_arn" {
   value = aws_iam_role.github_actions.arn
 }
+
+output "frontend_public_ip" {
+  value = aws_instance.frontend_server.public_ip
+}
+
+output "backend_private_ip" {
+  value = aws_instance.backend_server.private_ip
+}
+
+output "frontend_instance_id" {
+  value = aws_instance.frontend_server.instance_id
+}
+
+output "backend_instance_id" {
+  value = aws_instance.backend_server.instance_id
+}
+
+
