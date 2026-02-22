ea4910ceaa2498001374e7d939bbd0d42af175f6
olaniyi <ayodelebwj@gmail.com>
2026-02-22 13:39:38 -0500

    vfr

---
+++ terraform/outputs.tf
@@ -0,0 +1,20 @@
+
+      output frontend_pub_ip {
+        value = aws_instance.frontend_server.public_ip
+      }
+
+      output  backend_pri_ip {
+        value = aws_instance.backend_server.private_ip
+      }
+
+      output frontend_instance_id {
+        value = aws_instance.frontend_server.id
+      }
+
+      output backend_instance_id {
+        value = aws_instance.backend_server.id
+      }
+
+      output role_arn {
+        value = aws_iam_role.ssm_role.arn
+      }
