11cb262275270f994223d541331bceb88d6eb2d9
olaniyi <ayodelebwj@gmail.com>
2026-02-22 13:32:08 -0500

    xsw

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -2,6 +2,6 @@ data "aws_subnet" "private_subnet" {
   id = "subnet-0c6ab2a40abf8d516"
 }
 
-data "aws_subnet" "private_subnet" {
+data "aws_subnet" "public_subnet" {
   id = "subnet-0e7c30f99b3fd1f62"
 }
