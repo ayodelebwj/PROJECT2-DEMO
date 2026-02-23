88e6d9566b4698a2b3017158054fa6ec6a0b75a5
olaniyi <ayodelebwj@gmail.com>
2026-02-22 22:36:00 -0500

    vfccghghc

--- terraform/git_action_role.tf
+++ terraform/git_action_role.tf
@@ -1,3 +1,11 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+  
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
 resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-role"
 
@@ -7,7 +15,7 @@ resource "aws_iam_role" "github_actions_role" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = "arn:aws:iam::<380029909039>:oidc-provider/token.actions.githubusercontent.com"
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
@@ -15,7 +23,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:ayodelebwj/PROJECT2-DEMO:*"
+            "token.actions.githubusercontent.com:sub" = "repo:YOUR_GITHUB_ORG/YOUR_REPO:*"
           }
         }
       }
