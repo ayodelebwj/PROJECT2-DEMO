465ad4414268982e053cbdf22bc180ff6620ce6a
olaniyi <ayodelebwj@gmail.com>
2026-02-22 22:21:38 -0500

    hbjhgvhvhv

--- terraform/git_action_role.tf
+++ terraform/git_action_role.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "github_actions_role_arn" {
+resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-role"
 
   assume_role_policy = jsonencode({
@@ -47,6 +47,6 @@ resource "aws_iam_policy" "github_actions_ec2_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "attach_ec2_policy" {
-  role       = aws_iam_role.github_actions_role_arn.name
+  role       = aws_iam_role.github_actions_role.name
   policy_arn = aws_iam_policy.github_actions_ec2_policy.arn
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -16,6 +16,6 @@
       }
 
       output role_arn {
-        value = aws_iam_role.github_actions_role_arn.arn
+        value = aws_iam_role.github_actions_role.arn
       }
       
