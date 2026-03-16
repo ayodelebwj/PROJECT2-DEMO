25a0fd5e1aa22317f4d052b5df8b502759c0c951
olaniyi <ayodelebwj@gmail.com>
2026-03-16 16:54:50 -0400

    hghvgvgh

--- backup/terraform/terraform/subnet.tf
+++
@@ -1,7 +0,0 @@
-data "aws_subnet" "private_subnet" {
-  id = var.private_subnet_id
-}
-
-data "aws_subnet" "public_subnet" {
-  id = var.public_subnet_id
-}
--- backup/terraform/terraform/vpc.tf
+++
@@ -1,3 +0,0 @@
-data "aws_vpc" "myvpc" {
-  id = var.vpc_id
-}
